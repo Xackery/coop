006e48ea76b5ce3e7cbc0e3d48bca649af821dd3
Logan Smith <xackery@gmail.com>
2014-09-16 20:13:55 -0700

    Removed login details.

--- application/classes/Controller/Login.php
+++ application/classes/Controller/Login.php
@@ -9,17 +9,6 @@ class Controller_Login extends Template_Core {
 	
 	public function action_index()
 	{
-		if (Auth::instance()->logged_in('login')) $this->redirect('member/index');
-
-		if (!Auth::instance()->login(Arr::get($post, 'email', ''), Arr::get($post, 'password', ''))) {
-			$this->template->loginMessage = SESSION::instance()->get('message');
-		} else {
-			if (Auth::instance()->logged_in('admin')) $this->redirect('/admin/index');
-			else if (Auth::instance()->logged_in('login')) $this->redirect('member/index');
-
-			$curUser = Auth::instance()->get_user();
-		    $curUser->last_login = date('Y-m-d H:i:s');
-			$curUser->update();
-		}
+		echo "Login";
 	}		
 }
