214947137c2c2a894c460c838f086641d7758a55
Logan Smith <xackery@gmail.com>
2014-09-18 19:12:10 -0700

    Prelim auth system

---
+++ application/classes/Model/Auth.php
@@ -0,0 +1,12 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.');
+
+class Model_Auth extends Model {
+	/**
+	* Insert a new game into database
+	* @return row number inserted or error
+	*/
+	public function insert($name) {
+		
+	}
+
+}
